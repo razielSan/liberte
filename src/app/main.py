8a666f7234235075e834a2fa960d35648cb08735
raziel <tvindzon@gmail.com>
2026-01-13 20:22:23 +0700

    refactor: src.app.main.py

--- src/app/main.py
+++ src/app/main.py
@@ -4,7 +4,7 @@ import sys
 
 from app.bot.main import run_bot
 from app.bot.core.bot import telegram_bot
-from app.settings.init_logging import root_warning_logger, root_info_logger
+from app.core.init_logging import root_warning_logger, root_info_logger
 
 # Меняет тип event_loop для виндоус чтобы при нажатии ctl+c не было ошибки KeyboardInterrupt
 if sys.platform == "win32":
@@ -61,45 +61,3 @@ async def _run_windows():
         except Exception as err:
             root_warning_logger.warning(f"Ошибка при закрытии сессии: {err}")
         root_info_logger.info("Приложение завершило работу корректно")
-
-
-# async def main() -> None:
-#     """Запуск основного приложения."""
-#     # Если платформа ни виндоус
-#     if sys.platform != "win32":
-#         # корректно завершает работу на сервере
-#         loop = asyncio.get_event_loop()
-#         stop_event = asyncio.Event()
-
-#         for sig in (signal.SIGINT, signal.SIGTERM):
-#             loop.add_signal_handler(sig, stop_event.set)
-
-#         root_info_logger.info("Приложение запущен(Unix mode)")
-#         await asyncio.gather(run_bot(), stop_event.wait())
-#     else:
-#         try:
-#             # Запускам бота
-#             root_info_logger.info("Приложение запущено (Windows mode)")
-#             await run_bot()
-#         except Exception:
-#             pass
-#         finally:
-#             # Завершаем работy для windows
-#             root_info_logger.info("Приложение завершает работу")
-#             try:
-#                 if getattr(telegram_bot, "session", None):
-#                     await telegram_bot.session.close()  # аккуратно закрываем сессию
-#             except RuntimeError:
-#                 root_warning_logger.warning(
-#                     "Сессия уже была закрыта или event loop завершен"
-#                 )
-#             except Exception as err:
-#                 root_warning_logger.warning(f"Ошибка при закрытии сессии: {err}")
-#             root_info_logger.info("Приложение завершило работу корректно")
-
-
-# if __name__ == "__main__":
-#     try:
-#         asyncio.run(main())
-#     except KeyboardInterrupt:  # Проглатываем ошибку KeyboardInterrupt для windows чтобы не отображалась
-#         root_info_logger.info("Приложение остановлено вручную (Ctrl+C)")
