5b3b5d6cd25b40605e2a6fb1348275e2f2c431a3
raziel <tvindzon@gmail.com>
2026-01-13 20:29:05 +0700

    feat: src/app/core/paths.py

--- src/app/core/paths.py
+++ src/app/core/paths.py
@@ -1,5 +1,15 @@
 from pathlib import Path
-import app
+from dataclasses import dataclass
 
-APP_DIR:  Path = Path(app.__file__).resolve().parent
-SRC_DIR: Path = APP_DIR.parent
+from core.paths.paths import SRC_DIR
+
+
+@dataclass
+class AppPath:
+    APP_DIR: Path = SRC_DIR / "app"
+    LOG_DIR: Path = APP_DIR / "logs"
+    STATIC_DIR: Path = APP_DIR / "static"
+    TEMP_DIR: Path = APP_DIR / "temp"
+
+
+app_path: AppPath = AppPath()
