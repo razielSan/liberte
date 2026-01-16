cc6c1cf59d1cef6bdcf68aba053db89be7d4f3c7
raziel <tvindzon@gmail.com>
2026-01-16 15:20:50 +0700

    refactor: src/app/settings.py

--- src/app/settings.py
+++ src/app/settings.py
@@ -4,4 +4,7 @@ from pydantic import BaseModel
 class AppSettings(BaseModel):
     """Общие настроцки для всего приложения."""
 
-    BOT_ROOT_NAME: str = "root_bot"
+    SERVICE_NAME: str = "root_bot"
+
+
+settings = AppSettings()
