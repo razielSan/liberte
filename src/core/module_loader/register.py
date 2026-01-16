1099975572fca911609936f20894bd066e0e63b3
raziel <tvindzon@gmail.com>
2026-01-16 15:33:24 +0700

    refactor: src\core\module_loader\register.py

--- src/core/module_loader/register.py
+++ src/core/module_loader/register.py
@@ -1,12 +1,13 @@
 from typing import List, Dict
 
-from aiogram import Dispatcher
 from core.response.modules_loader import ModuleInfo
+from core.response.response_data import LoggingData
 
 
 def register_module(
-    dp: Dispatcher,
+    dp: object,
     modules: List[ModuleInfo],
+    logging_data: LoggingData,
 ) -> None:
     """
     Подключает к диспетчеру переданные роутеры.
@@ -14,30 +15,35 @@ def register_module(
 
     Args:
         dp (Dispatcher): Диспетчер aiogram
-        modules (List[ModuleInfo]): Список содержащий в себе обьект класса ModuleInfo 
+        modules (List[ModuleInfo]): Список содержащий в себе обьект класса ModuleInfo
         для подключения router
     """
 
-    root: List[ModuleInfo] = sorted([m for m in modules if m.is_root], key=lambda m: m.root)
+    root: List[ModuleInfo] = sorted(
+        [m for m in modules if m.is_root], key=lambda m: m.root
+    )
     children: List[ModuleInfo] = sorted(
         [m for m in modules if not m.is_root], key=lambda m: m.module_depth
     )
     children = sorted(
         children,
         key=lambda m: m.parent,
-    ) 
+    )
 
     root_map: Dict = {}
 
     for mod in root:
         dp.include_router(router=mod.router.router)
         root_map[mod.root] = mod.router.router
-        print("\n[Auto] Root router inculde into dp: {}".format(mod.router.router))
+        logging_data.info_logger.info(
+            "\n[Auto] Root router inculde into dp: {}".format(mod.router.router)
+        )
+
     for mod in children:
         parent_router = root_map.get(mod.parent)
         if not parent_router:
             continue
         parent_router.include_router(mod.router.router)
-        print(
-            f"\n[Auto] Child router inculded into {parent_router}: {mod.router.router}"
+        logging_data.info_logger.info(
+            f"\n[Auto] Child router inculded into {parent_router}: {mod.router.router}",
         )
