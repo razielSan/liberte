38f7db5c65ea951b890a611ddc4590343ab3427a
raziel <tvindzon@gmail.com>
2026-01-16 15:21:25 +0700

    refactor: src/app/bot/main.py

--- src/app/bot/main.py
+++ src/app/bot/main.py
@@ -1,21 +1,27 @@
 import aiohttp
 
 from app.bot.core.startup import setup_bot
+from app.bot.settings import settings
 from app.bot.core.bot import telegram_bot
-from app.bot.core.init_logging import bot_error_logger, bot_info_logger
+from core.logging.api import get_loggers
 
 
 async def run_bot() -> None:
     """Подлючает все параметры для бота и запускает его."""
     # Встаем в try/except чтобы отловить все что не попало в middleware
     try:
+
         dp = await setup_bot()
+        logging_data = get_loggers(name=settings.SERVICE_NAME)
+
         # Создаем глобальную сессию для всего бота. Будет доступ в роутерах через
         # название указанное ниже
         async with aiohttp.ClientSession() as session:
             dp["session"] = session
-            bot_info_logger.info("bot запущен")
+            logging_data.info_logger.info("bot запущен")
             await dp.start_polling(telegram_bot)
 
-    except Exception as err:
-        bot_error_logger.exception(f"Критическая ошибка при работа бота - bot: {err}")
+    except Exception:
+        logging_data.info_logger.exception(
+            f"Критическая ошибка при работа бота - {settings.SERVICE_NAME}"
+        )
