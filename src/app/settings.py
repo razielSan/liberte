47118c477728b381b9a488f7bb110ef1aa23b130
raziel <tvindzon@gmail.com>
2026-01-18 15:48:28 +0700

    refactor: src/app/settings.py

--- src/app/settings.py
+++ src/app/settings.py
@@ -5,6 +5,7 @@ class AppSettings(BaseModel):
     """Общие настроцки для всего приложения."""
 
     SERVICE_NAME: str = "root_bot"
+    NAME_FOR_LOG_FOLDER: str = "root_bot"
 
 
-settings = AppSettings()
+settings: AppSettings = AppSettings()
