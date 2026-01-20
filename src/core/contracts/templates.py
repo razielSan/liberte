c4dd70b5ef8a014529f8ab1d1718e8416bab28d5
raziel <tvindzon@gmail.com>
2026-01-20 12:28:34 +0700

    refactor: src/core/contracts/templates.py

--- src/core/contracts/templates.py
+++ src/core/contracts/templates.py
@@ -49,6 +49,7 @@ class ModuleSettings(BaseModel):
     MENU_REPLY_TEXT: str = "{name}" 
     MENU_CALLBACK_TEXT: str = "{name}"
     MENU_CALLBACK_DATA: str = "{name}"
+    SHOW_IN_MAIN_MENU: bool = True
     NAME_FOR_LOG_FOLDER: str = "{log_name}"
     NAME_FOR_TEMP_FOLDER: str = "{temp_path}"
     ROOT_PACKAGE: str = "{root_package}"
@@ -61,12 +62,24 @@ from pathlib import Path
 from core.module_loader.loader import get_child_modules_settings_inline_data
 from app.app_utils.keyboards import get_total_buttons_inline_kb
 from app.bot.core.paths import bot_path
+from core.logging.api import get_loggers
+
+
+logging_data = get_loggers(name="{log_name}")
+
 
 
 inline_data = get_child_modules_settings_inline_data(
     module_path=bot_path.BOT_DIR / "modules" / Path("{path_to_module}"),
-    root_package="{root_childes}"
+    root_package="{root_childes}",
+    logging_data=logging_data,
 )
+if len(inline_data) >= 1:
+    buttons_array = [button.text for button in inline_data]
+    buttons = ", ".join(buttons_array)
+    logging_data.info_logger.info(
+        msg=f"[CREATE INLINE KEYBOARDS] Инлайн клавиатура для модуля test создана\\nКнопки - {buttons}"
+    )
 
 get_keyboards_menu_buttons = get_total_buttons_inline_kb(
     list_inline_kb_data=inline_data, quantity_button=1
