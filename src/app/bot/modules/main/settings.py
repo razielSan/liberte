25e72d295d277b60bd7a7f0327977bdfe2110fa1
raziel <tvindzon@gmail.com>
2026-01-20 12:27:37 +0700

    refacator: src/core/contracts/settings.py

--- src/app/bot/modules/main/settings.py
+++ src/app/bot/modules/main/settings.py
@@ -8,6 +8,7 @@ class MainRouterSettings(BaseModel):
     MENU_REPLY_TEXT: str = "main"
     MENU_CALLBACK_TEXT: str = "main"
     MENU_CALLBACK_DATA: str = "main"
+    SHOW_IN_MAIN_MENU: bool = False
     NAME_FOR_TEMP_FOLDER: str = "main"
     NAME_FOR_LOG_FOLDER: str = "main"
     ROOT_PACKAGE: str = "app.bot.modules.main"
