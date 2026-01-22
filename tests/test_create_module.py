9187a75814935930c29961326bcd2b80eb0f41ab
raziel <tvindzon@gmail.com>
2026-01-22 13:28:52 +0700

    refactor: tests\test_create_module.py

--- tests/test_create_module.py
+++ tests/test_create_module.py
@@ -1,19 +1,16 @@
 import pytest
 
-from core.module_loader.creator import create_module
+from core.module_loader.scaffolding.creator import create_module
 
 
 @pytest.mark.parametrize(
     "module",
-    [
-        "test",
-        "test.test",
-        "data.test.data.test.data.data.test.test",
-    ],
+    ["test", "test.test.test", "test.test.test.test.data.test.data"],
 )
 def test_create_module_success(tmp_path, module):
     root_dir = tmp_path
     root_dir.mkdir(parents=True, exist_ok=True)
+
     module_name = module
     root_package = "test_app.bot.modules"
 
@@ -22,7 +19,7 @@ def test_create_module_success(tmp_path, module):
         root_package=root_package,
         root_dir=tmp_path,
     )
-    assert response_create_module.message == "success"
+    assert response_create_module.data == "success"
     assert response_create_module.error is None
 
     module_path = (
@@ -33,7 +30,7 @@ def test_create_module_success(tmp_path, module):
     assert module_path.exists()
 
     # Основные файлы  должны существовать
-    for filename in ["router.py", "settings.py", "logging.py", "response.py"]:
+    for filename in ["router.py", "settings.py", "response.py"]:
         assert (module_path / filename).exists()
 
     # Основные директории
@@ -49,20 +46,23 @@ def test_create_module_success(tmp_path, module):
         assert (module_path / dirname).exists()
         assert ((module_path / dirname) / "__init__.py").exists()
 
-    # Проверяем что в router.py имя подставленно корректно
+    # Проверяем что переменные роутер и settings созданы в файлах
     router_file = module_path / "router.py"
     content = router_file.read_text(encoding="utf-8")
-    module_name_formatted = module_name.replace(".", ".childes.")
-    assert f"Router(name='{module_name_formatted}')" in content
+    assert "router" in content
+
+    settings_file = module_path / "settings.py"
+    content = settings_file.read_text(encoding="utf-8")
+    assert "settings" in content
 
 
 def test_two_create_module_success(tmp_path):
     """
     Проверяет корректно подставленных значений в первом модуле
     после создания второго
-    
+
     """
-    
+
     root_dir = tmp_path
     root_dir.mkdir(parents=True, exist_ok=True)
     root_package = "test_app.bot.modules"
@@ -75,7 +75,7 @@ def test_two_create_module_success(tmp_path):
         root_package=root_package,
         root_dir=tmp_path,
     )
-    assert response_create_module.message == "success"
+    assert response_create_module.data == "success"
     assert response_create_module.error is None
 
     first_module_path = (
@@ -94,7 +94,7 @@ def test_two_create_module_success(tmp_path):
         root_dir=tmp_path,
     )
 
-    assert response_create_module.message == "success"
+    assert response_create_module.data == "success"
     assert response_create_module.error is None
 
     second_module_path = (
@@ -104,16 +104,14 @@ def test_two_create_module_success(tmp_path):
     )
     assert second_module_path.exists()
 
-    # Проверяем что во 2 модуле в router.py имя подставленно корректно
-    second_router_file = second_module_path / "router.py"
+    # Проверяем что во 2 модуле в settings имена подставленны корректно
+    second_router_file = second_module_path / "settings.py"
     content = second_router_file.read_text(encoding="utf-8")
-    second_module_name_formatted = second_module_name.replace(".", ".childes.")
-    assert f"Router(name='{second_module_name_formatted}')" in content
+    assert "{" not in str(content)
+    assert "}" not in str(content)
 
-    # Проверка 1 модуля на подставление имени после создания второго
-    first_router_file = first_module_path / "router.py"
+    # Проверка 1 модуля на подставление имени после создания второго, settings
+    first_router_file = first_module_path / "settings.py"
     content = first_router_file.read_text(encoding="utf-8")
-    first_module_name_formatted = first_module_name.replace(".", ".childes.")
-    assert f"Router(name='{first_module_name_formatted}')" in content
-
-
+    assert "{" not in str(content)
+    assert "}" not in str(content)
