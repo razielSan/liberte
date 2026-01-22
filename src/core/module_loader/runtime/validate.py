b9cdd18819c3a0771ba6537201542fbf9796c010
raziel <tvindzon@gmail.com>
2026-01-22 13:23:25 +0700

    create: src/core/module_loader/runtime/validata.py

---
+++ src/core/module_loader/runtime/validate.py
@@ -0,0 +1,49 @@
+from types import ModuleType
+
+from core.contracts.constants import (
+    DEFAULT_NAME_SETTINGS,
+)
+from core.contracts.module import REQUIRED_FIELDS_MODULES
+from core.response.response_data import Result
+from core.error_handlers.helpers import ok, fail
+from core.error_handlers.helpers import safe_import
+
+
+def validate_module(
+    root_package: str,
+    required_field_modules: set = REQUIRED_FIELDS_MODULES,
+    name: str = DEFAULT_NAME_SETTINGS,
+    root: bool = None,
+) -> Result:
+
+    result_import = None
+    if root:
+        result_import: Result = safe_import(module_package=f"{root_package}")
+    else:
+        result_import: Result = safe_import(module_package=f"{root_package}.{name}")
+
+    if not result_import.ok:
+        return result_import
+
+    module_data: ModuleType = result_import.data
+
+    if not hasattr(module_data, name):
+        return fail(
+            code="Invalide module", message=f"{name} not found - {module_data}"
+        )
+
+    response_data = getattr(module_data, f"{name}")
+
+    modules_set = set(
+        response_data.model_fields.keys()
+    )  # получаем множество из имен полей модели
+    result_data = required_field_modules.difference(modules_set)
+    if (
+        result_data
+    ):  # если осталось хоть одно поле значит его нет в загруженной модели
+        return fail(
+            code="Invalide module",
+            message=f"Invalide module settings : {result_data} missing",
+        )
+
+    return ok(data=response_data)
